1a5c83b2a39d630d49de6518e3765669f50801e0
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-22 20:57:41 -0300

    chore: generate empty core and shared modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+
+// O que faz este modulo?
+// O AppModule é o modulo raiz da aplicação, ele importa os modulos SharedModule e CoreModule
+// e registra o AppController e o AppService. O AppModule é o ponto de entrada da aplicação NestJS.
 
 @Module({
-  imports: [],
+  imports: [SharedModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
