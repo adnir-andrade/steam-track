46f6d525cf9ac5196b714365eac28f64ea07464f
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-29 15:54:21 -0300

    style: Remove commentaries

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,6 @@ import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
-// O que faz este modulo?
-// O AppModule é o modulo raiz da aplicação, ele importa os modulos SharedModule e CoreModule
-// e registra o AppController e o AppService. O AppModule é o ponto de entrada da aplicação NestJS.
 
 @Module({
   imports: [SharedModule, CoreModule],
