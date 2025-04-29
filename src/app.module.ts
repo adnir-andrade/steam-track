0c0d1a93d1be97c2135ea27d97c45289c17d6948
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-29 15:55:16 -0300

    feat: Create games module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [SharedModule, CoreModule],
+  imports: [SharedModule, CoreModule, GamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
