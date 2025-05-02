07dde2430cd45fcec41f9dacc0f79abba3e05e33
Adnir Andrade <adnir_andrade@hotmail.com>
2025-05-02 09:44:43 -0300

    feat: Add global auth middleware to check for Authorization header

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { GamesModule } from './games/games.module';
 import { UsersModule } from './users/users.module';
+import {AuthMiddleware} from "./auth/auth.middleware";
 
 @Module({
   imports: [SharedModule, CoreModule, GamesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
---
+++ src/auth/auth.middleware.ts
@@ -0,0 +1,9 @@
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: 'Token não fornecido' });
+    }
+    next();
+  }
+}
