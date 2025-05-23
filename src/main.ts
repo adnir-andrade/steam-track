016de6ba25fa2d65b4ca50ac4febd60fbbdfdd0f
Adnir Andrade <adnir_andrade@hotmail.com>
2025-05-23 15:15:17 -0300

    feat: Update main to handle exceptions based on what was created

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {HttpExceptionFilter} from "./http-exception/http-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-      new ValidationPipe({
-        transform: true,
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        // @ts-ignore
-        enableImplicitConversion: true,
-      }),
-  );
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            // @ts-ignore
+            enableImplicitConversion: true,
+        }),
+    );
+
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    await app.listen(3000);
 }
 bootstrap();
