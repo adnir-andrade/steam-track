bf62b7696d5c04d0e519e2bdf0a75c604fb1680b
Adnir Andrade <adnir_andrade@hotmail.com>
2025-06-08 14:42:45 -0300

    feat: Generate AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { UsersModule } from './users/users.module';
 import {AuthMiddleware} from "./auth/auth.middleware";
 import { ErrorsModule } from './errors/errors.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SharedModule, CoreModule, GamesModule, UsersModule, ErrorsModule, PrismaModule],
+  imports: [SharedModule, CoreModule, GamesModule, UsersModule, ErrorsModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
